29068ebf62518d631cd6ee69d990b3ee16721187
Mateus <mateuscorreia788@gmail.com>
2025-04-29 20:04:10 -0300

    Revert "Atualização"
    This reverts commit 67a40ebf828aa288ceb96d9257d5af051d78da05.

--- app/src/main/java/com/example/projetofinal/screens/LoginScreen.kt
+++ app/src/main/java/com/example/projetofinal/screens/LoginScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -166,7 +165,6 @@ fun LoginScreen(navController: NavController, paddingValues: PaddingValues, data
                         }
                     }
                 }
-            }, colors = androidx.compose.material3.ButtonDefaults.buttonColors(containerColor = androidx.compose.material.MaterialTheme.colors.blue_button),
             },
             modifier = Modifier
                 .width(400.dp)
