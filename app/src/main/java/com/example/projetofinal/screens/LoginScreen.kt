67a40ebf828aa288ceb96d9257d5af051d78da05
mateuscorreia788 <mateuscorreia788@gmail.com>
2025-04-29 22:58:41 +0000

    Atualização

--- app/src/main/java/com/example/projetofinal/screens/LoginScreen.kt
+++ app/src/main/java/com/example/projetofinal/screens/LoginScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -165,6 +166,7 @@ fun LoginScreen(navController: NavController, paddingValues: PaddingValues, data
                         }
                     }
                 }
+            }, colors = androidx.compose.material3.ButtonDefaults.buttonColors(containerColor = androidx.compose.material.MaterialTheme.colors.blue_button),
             },
             modifier = Modifier
                 .width(400.dp)
